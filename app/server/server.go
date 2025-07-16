c0bd732901764c2d01fa04854186a989643087d2
avdes96 <avdes96@gmail.com>
2025-07-16 23:24:59 +0100

    Moved server replica sync functions to separate file

--- app/server/server.go
+++ app/server/server.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"log"
 	"net"
-	"strings"
 
 	"github.com/codecrafters-io/redis-starter-go/app/command"
 	"github.com/codecrafters-io/redis-starter-go/app/protocol"
@@ -52,114 +51,6 @@ func New(configParams map[string]string, replInfo *utils.ReplicationInfo) (*redi
 		currentDatabase: 0,
 		replicationInfo: replInfo,
 	}, nil
-}
-
-func (r *redisServer) SyncWithMaster() {
-	if r.replicationInfo.Role == utils.MASTER {
-		return
-	}
-	conn, err := net.Dial("tcp", r.replicationInfo.MasterAddress)
-	if err != nil {
-		log.Printf("Error dialing master server: %s", err)
-		return
-	}
-
-	if err = r.initiateConnection(conn); err != nil {
-		log.Printf("Error initiating connection with master server: %s", err)
-		return
-	}
-
-	if err = r.configureReplica(conn); err != nil {
-		log.Printf("Error configuring replica: %s", err)
-		return
-	}
-
-	if err = r.initialiseReplicationStream(conn); err != nil {
-		log.Printf("Error initialising replication stream: %s", err)
-		return
-	}
-}
-
-func (r *redisServer) initiateConnection(conn net.Conn) error {
-	_, err := conn.Write([]byte(protocol.ToArrayBulkStrings([]string{"PING"})))
-	if err != nil {
-		return err
-	}
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		return err
-	}
-	if string(buf[:n]) != "+PONG\r\n" {
-		return fmt.Errorf("expected %s, got %s", "+PONG\r\n", string(buf[:n]))
-	}
-	return nil
-}
-
-func (r *redisServer) configureReplica(conn net.Conn) error {
-	port := r.configParams["port"]
-	if port == "" {
-		return nil
-	}
-	_, err := conn.Write([]byte(protocol.ToArrayBulkStrings([]string{
-		"REPLCONF", "listening-port", port,
-	})))
-	if err != nil {
-		return err
-	}
-
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		return err
-	}
-	if string(buf[:n]) != "+OK\r\n" {
-		return fmt.Errorf("expected %s, got %s", "+OK\r\n", string(buf[:n]))
-	}
-
-	_, err = conn.Write([]byte(protocol.ToArrayBulkStrings([]string{
-		"REPLCONF", "capa", "psync2",
-	})))
-	if err != nil {
-		return err
-	}
-
-	buf = make([]byte, 1024)
-	_, err = conn.Read(buf)
-	if err != nil {
-		return err
-	}
-	if string(buf[:n]) != "+OK\r\n" {
-		return fmt.Errorf("expected %s, got %s", "+OK\r\n", string(buf[:n]))
-	}
-	return nil
-}
-
-const fullresync string = "+FULLRESYNC"
-
-func (r *redisServer) initialiseReplicationStream(conn net.Conn) error {
-	_, err := conn.Write([]byte(protocol.ToArrayBulkStrings([]string{
-		"PSYNC", "?", "-1",
-	})))
-	if err != nil {
-		return err
-	}
-
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		return err
-	}
-	if !strings.HasPrefix(string(buf[:n]), fullresync) {
-		return fmt.Errorf("expected resp to start with %s, got %s", fullresync, buf[:n])
-	}
-	buf = make([]byte, 1024)
-	_, err = conn.Read(buf)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func (r *redisServer) Run() error {
